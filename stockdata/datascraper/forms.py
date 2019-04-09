22a85f4baeb23e3ad18a52f17e4469d134824e02
Matthew Derango <mderango7@berkeley.edu>
2019-04-09 02:12:06 -0700

    started form functionality for web app

---
+++ stockdata/datascraper/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class QueryForm(forms.Form):
+    query = forms.CharField(max_length=10)
+
--- stockdata/datascraper/views.py
+++ stockdata/datascraper/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
-# Create your views here.
+from .forms import QueryForm
+
+import matplotlib
+matplotlib.use('Agg')
+
+
+def query(request):
+    form = QueryForm()
+    return render(request, 'datascraper/form.html', {'form': form})
